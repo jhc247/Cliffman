7e900acca4f9ae653c2de9407aaa6b051e06668e
Jan Cardenas <jhc247@cornell.edu>
2014-08-07 14:59:44 -0500

    Initial Commit

---
+++ Ropeman/Classes/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  Ropeman
+//
+//  Created by Jcard on 8/7/14.
+//  Copyright JCard 2014. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+#import "cocos2d.h"
+
+@interface AppDelegate : CCAppDelegate
+@end
